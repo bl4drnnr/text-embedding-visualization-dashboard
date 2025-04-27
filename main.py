539f0258c644f999d6cd1406f842ecaf7bd91408
bl4drnnr <mikhail.bahdashych@protonmail.com>
2025-04-27 21:33:16 +0200

    embeddings small refactor

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from src.text_embedding_visualization_dashboard.embeddings import Embeddings
 from src.text_embedding_visualization_dashboard.vector_db.db import VectorDB
 
@@ -6,14 +7,36 @@ def main():
     vector_db = VectorDB()
     service = Embeddings(vector_db)
 
-    texts = ["I love pizza!", "The sun is shining.", "I hate rainy days."]
-    service.batch_process_texts(texts=texts, collection_name="test_embeddings")
+    def upload_goemotion_data():
+        data_files = ["data/goemotions_1.csv", "data/goemotions_2.csv", "data/goemotions_3.csv"]
+        dfs = [pd.read_csv(file) for file in data_files]
+        df = pd.concat(dfs, ignore_index=True)
+        
+        texts = df["text"].tolist()
 
-    query = "I really enjoy eating pizza."
-    results = service.query_similar_texts(query_text=query, collection_name="test_embeddings")
+        if "labels" in df.columns:
+            metadatas = [{"label": str(label)} for label in df["labels"]]
+        else:
+            metadatas = None
 
-    for idx, item in enumerate(results):
-        print(f"{idx+1}. {item['text']} (Distance: {item['distance']:.4f})")
+        service.batch_process_texts(
+            texts=texts,
+            collection_name="goemotions_embeddings",
+            metadatas=metadatas,
+            batch_size=128
+        )
+
+    def query_goemotion_data(query: str):
+        results = service.query_similar_texts(query_text=query, collection_name="goemotions_embeddings", top_k=5)
+
+        print(f"\nWyniki dla zapytania: \"{query}\":\n")
+        for idx, item in enumerate(results):
+            print(f"{idx+1}. {item['text']} (Distance: {item['distance']:.4f}) Metadata: {item['metadata']}")
+
+    # Zauploadujcie sobie dane jeden raz i można to zakomentować
+    upload_goemotion_data()
+    # Tutaj możecie sobie podstawić jakieś query i pobawić się tym
+    query_goemotion_data("I like Canada.")
 
 
 if __name__ == "__main__":
