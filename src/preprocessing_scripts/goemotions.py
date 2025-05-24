0b7573da6267ac847a4ef903e06a49af6dae289b
jakub <konieczny.jakub@onet.pl>
2025-05-24 19:26:39 +0200

    update

--- src/preprocessing_scripts/goemotions.py
+++ src/preprocessing_scripts/goemotions.py
@@ -1,4 +1,14 @@
 import pandas as pd
+import numpy as np
+
+
+def get_emotions(row, emotion_columns):
+    emotions = [emotion for emotion in emotion_columns if row[emotion] == 1]
+
+    if len(emotions) == 1:
+        return emotions[0]
+    else:
+        return np.nan
 
 
 def preprocess_goemotions(dataset_id):
@@ -8,10 +18,8 @@ def preprocess_goemotions(dataset_id):
 
     emotion_columns = df.columns[df.columns.get_loc("admiration") :]
 
-    df_processed["label"] = df[emotion_columns].apply(
-        lambda row: "-".join([emotion for emotion in emotion_columns if row[emotion] == 1]), axis=1
-    )
-    df_processed = df_processed[df_processed["label"] != ""]
+    df_processed["label"] = df[emotion_columns].apply(lambda row: get_emotions(row, emotion_columns), axis=1)
+    df_processed.dropna(inplace=True)
     df_processed.to_csv(f"../../data/goemotions_processed_{dataset_id}.csv", index=False)
 
 
